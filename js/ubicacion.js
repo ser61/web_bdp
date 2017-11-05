c7362dc1ee68b6507dc099f52064e80e2f32f83c
sergio <ser61_ol@yahoo.es>
2017-11-05 09:51:16 -0400

    mapas en modal en la pagina principal listo...

--- js/ubicacion.js
+++ js/ubicacion.js
@@ -1,6 +1,5 @@
 var marker;
-var coords = {};
-
+var markerS;
 initMap = function (){
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -17.343778, lng: -63.248016},
@@ -17,11 +16,48 @@ initMap = function (){
     marker.addListener('click', toggleBounce);
 }
 
+function montero() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -17.343778, lng: -63.248016},
+        zoom: 15
+    });
+
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: new google.maps.LatLng(-17.343778,-63.248016),
+
+    });
+    marker.addListener('click', toggleBounce);
+}
+
+function santaCruz() {
+    mapS = new google.maps.Map(document.getElementById('map-s'), {
+        center: {lat: -17.775419, lng: -63.183382},
+        zoom: 15
+    });
+
+    markerS = new google.maps.Marker({
+        map: mapS,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: new google.maps.LatLng(-17.775419,-63.183382),
+
+    });
+    markerS.addListener('click', toggleBounce);
+}
+
 //callback al hacer clic en el marcador lo que hace es quitar y poner la animacion BOUNCE
 function toggleBounce() {
     if (marker.getAnimation() !== null) {
         marker.setAnimation(null);
     } else {
         marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+    if (markerS.getAnimation() !== null) {
+        markerS.setAnimation(null);
+    } else {
+        markerS.setAnimation(google.maps.Animation.BOUNCE);
     }
 }
